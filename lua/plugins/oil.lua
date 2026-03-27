6add4020f2f5cadb4ed36f18cfc39a506c346ff6
João Costa <jvcosta1502@gmail.com>
2026-03-26 21:45:29 -0300

    atualizacao oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,42 +1,53 @@
 return {
-    -- ─── Desativa o Neo-tree ────────────────────
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        enabled = false,
-    },
-
-    -- ─── Oil.nvim ───────────────────────────────
-    {
-        "stevearc/oil.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        lazy = false,
-        keys = {
-            { "-",         "<cmd>Oil<CR>",   desc = "Oil (cwd)" },
-            { "<leader>e", "<cmd>Oil .<CR>", desc = "Oil (root)" },
-        },
-        opts = {
-            default_file_explorer         = true,
-            delete_to_trash               = true,
-            skip_confirm_for_simple_edits = true,
-            view_options                  = {
-                show_hidden = true,
-            },
-            float                         = {
-                padding    = 2,
-                max_width  = 80,
-                max_height = 30,
-                border     = "rounded",
-            },
-            win_options                   = {
-                wrap          = false,
-                signcolumn    = "no",
-                cursorcolumn  = false,
-                foldcolumn    = "0",
-                spell         = false,
-                list          = false,
-                conceallevel  = 3,
-                concealcursor = "nvic",
-            },
-        },
-    },
+	-- ─── Desativa o Neo-tree ────────────────────
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		enabled = false,
+	},
+	-- ─── Oil.nvim ───────────────────────────────
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		lazy = false,
+		keys = {
+			{
+				"-",
+				function()
+					require("oil").toggle_float()
+				end,
+				desc = "Oil flutuante (cwd)",
+			},
+			{
+				"<leader>e",
+				function()
+					require("oil").toggle_float(".")
+				end,
+				desc = "Oil flutuante (root)",
+			},
+		},
+		opts = {
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+			},
+			float = {
+				padding = 2,
+				max_width = 80,
+				max_height = 30,
+				border = "rounded",
+			},
+			win_options = {
+				wrap = false,
+				signcolumn = "no",
+				cursorcolumn = false,
+				foldcolumn = "0",
+				spell = false,
+				list = false,
+				conceallevel = 3,
+				concealcursor = "nvic",
+			},
+		},
+	},
 }
