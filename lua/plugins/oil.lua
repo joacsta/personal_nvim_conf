2dbef88979c3a197c1578c9fbfbffdce68f7e329
João Costa <jvcosta1502@gmail.com>
2026-04-02 22:27:07 -0300

    alteracao no oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,44 +1,51 @@
 return {
-        -- ─── Oil.nvim ───────────────────────────────
-    {
-        "stevearc/oil.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        lazy = false,
-        keys = {
-            {
-                "-",
-                function() require("oil").toggle_float() end,
-                desc = "Oil flutuante (cwd)",
-            },
-            {
-                "<leader>e",
-                function() require("oil").toggle_float(".") end,
-                desc = "Oil flutuante (root)",
-            },
-        },
-        opts = {
-            default_file_explorer         = true,
-            delete_to_trash               = true,
-            skip_confirm_for_simple_edits = true,
-            view_options                  = {
-                show_hidden = true,
-            },
-            float                         = {
-                padding    = 2,
-                max_width  = 80,
-                max_height = 30,
-                border     = "rounded",
-            },
-            win_options                   = {
-                wrap          = false,
-                signcolumn    = "no",
-                cursorcolumn  = false,
-                foldcolumn    = "0",
-                spell         = false,
-                list          = false,
-                conceallevel  = 3,
-                concealcursor = "nvic",
-            },
-        },
-    },
+	-- ─── Oil.nvim ───────────────────────────────
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		lazy = false,
+		keys = {
+			{
+				"-",
+				function()
+					require("oil").toggle_float()
+				end,
+				desc = "Oil flutuante (cwd)",
+			},
+			{
+				"<leader>e",
+				function()
+					require("oil").toggle_float(".")
+				end,
+				desc = "Oil flutuante (root)",
+			},
+			{
+				["q"] = "actions.close",
+			},
+		},
+		opts = {
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+			},
+			float = {
+				padding = 2,
+				max_width = 80,
+				max_height = 30,
+				border = "rounded",
+			},
+			win_options = {
+				wrap = false,
+				signcolumn = "no",
+				cursorcolumn = false,
+				foldcolumn = "0",
+				spell = false,
+				list = false,
+				conceallevel = 3,
+				concealcursor = "nvic",
+			},
+		},
+	},
 }
